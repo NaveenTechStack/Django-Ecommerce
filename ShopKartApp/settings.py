3ac5d251b5508e9e258d6f9a82af77acd0c64d75
Naveen-1195 <mailnaveenkumar95@gmail.com>
2026-03-01 10:19:10 +0530

    add postgres sql

--- ShopKartApp/settings.py
+++ ShopKartApp/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'ShopKartApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
